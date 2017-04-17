a271ef44e64fcbb42887fd2f163bca42b7f4defe
leangseu <leangseu_kim@student.uml.edu>
2017-04-17 13:01:44 -0400

    fix sound file name and sound rkt file

--- sound.rkt
+++ sound.rkt
@@ -3,27 +3,35 @@
 (require rsound)
 
 ;;It currently has lots of restrictions (it insists on 16-bit PCM encoding, for instance).
-(define play (rs-read "/Users/Chhayhout/Desktop/Chopping.wav"))
-;(rs-read path) 
-(define (sound)
-  (list
-   (list 'chopping (rs-read "/Users/Chhayhout/Desktop/Chopping.wav")
-   (list 'gameover (rs-read "/Users/Chhayhout/Desktop/Game-over.wav")))))
+(define sound-chopping (rs-read "Chopping.wav"))
+;(rs-read path)
+(define sound-gameover (rs-read "Game-over.wav"))
 
-(define (music)
-  (list
-   (list 'menu (rs-read "/Users/Chhayhout/Desktop/menu.wav"))
-   (list 'ingame (rs-read "/Users/Chhayhout/Desktop/ingame.wav"))))
+;;(define (sound)
+;;  (list
+;;   (list 'chopping (rs-read "Chopping.wav")
+;;   (list 'gameover (rs-read "Game-over.wav")))))
+
+(define sound-menu (rs-read "menu.wav"))
+(define sound-ingame (rs-read "ingame.wav"))
+
+;;(define music
+;;  (list
+;;   (list 'menu (rs-read "menu.wav"))
+;;   (list 'ingame (rs-read "ingame.wav"))))
  
-(define (play-forever sound)
+;;(define (play-forever sound)
   ;(make-pstream [#:buffer-time buffer-time]) → pstream? Create a new pstream and start playing it.
-  (define play (make-pstream))
+;;  (define play (make-pstream))
   ;(rs-frames sound) → nonnegative-integer? Returns the length of a sound, in frames.
-  (define length (rs-frames sound))
-  (let looping ((time 1))
+;;  (define length (rs-frames sound))
+;;  (let looping ((time 1))
     ;(pstream-queue pstream rsound frames) → pstream? Queue the given sound to be played at the time specified by frames.
-    (pstream-queue play sound (+ time 44100))
-    (define next (+ time length))
-    (looping next)))
+;;    (pstream-queue play sound (+ time 44100))
+;;    (define next (+ time length))
+;;    (looping next)))
 
-(play-forever play) 
+;;(play-forever play) 
+
+;; to play music
+;;(pstream-queue (make-pstream) <name> 44100)
