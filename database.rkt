187eb39887e29715e36785a8fe058df0595c374d
leangseu <leangseu_kim@student.uml.edu>
2017-04-18 23:08:21 -0400

    change sound lib to gui/base play sound. It run faster

--- database.rkt
+++ database.rkt
@@ -4,20 +4,21 @@
 
 (require db)
 
-(define db (sqlite3-connect #:database "hs.db"
+(define data (sqlite3-connect #:database "hs.db"
                            #:mode 'create))
 
+;; if table already exist update the table, else create the table
+(when (not (table-exists? data "hs"))
+  (query-exec data "create table hs(name text primary key, score integer)"))
 
 (define (update-table name score)
-  ;; if table already exist update the table, else create the table
-  (when (not (table-exists? db "hs"))
-    (query-exec db "create table hs(name text primary key, score integer)"))
-  ;;(query-exec db "insert or replace into hs values('k', 300)"))
-  (query-exec db (concat-string name score)))
+  ;;(query-exec data "insert or replace into hs values('k', 300)"))
+  (query-exec data (concat-string name score)))
 
 ;; display score from highest to lowest
 (define (display-score)
-  (for ([(name score) (in-query db "select * from hs order by score desc")]) (printf "~a\t: ~a\n" name score)))
+  (for ([(name score) (in-query data "select * from hs order by score desc")]) (printf "~a\t: ~a\n" name score)))
 
 (define (concat-string name score)
     (~a "insert or replace into hs values('" name "', " score ")"))
+
--- sound.rkt
+++ sound.rkt
@@ -2,20 +2,14 @@
 ;; export
 (provide (all-defined-out))
 
-(require rsound)
+(require racket/gui/base)
 
-;;It currently has lots of restrictions (it insists on 16-bit PCM encoding, for instance).
-(define sound-chopping (rs-read "assets/sounds/Chopping-edit.wav"))
-;(rs-read path)
-(define sound-gameover (rs-read "assets/sounds/Game-over-edit.wav"))
 
-;;(define (sound)
-;;  (list
-;;   (list 'chopping (rs-read "Chopping.wav")
-;;   (list 'gameover (rs-read "Game-over.wav")))))
+(define (sound-chopping) (play-sound "assets/sounds/Chopping-edit.wav" #t))
+(define (sound-gameover) (play-sound "assets/sounds/Game-over-edit.wav" #t))
 
-(define sound-menu (rs-read "assets/sounds/menu-edit.wav"))
-(define sound-ingame (rs-read "assets/sounds/ingame-edit.wav"))
+(define (sound-menu) (play-sound "assets/sounds/menu-edit.wav" #t))
+(define (sound-ingame) (play-sound "assets/sounds/ingame-edit.wav" #t))
 
 ;;(define music
 ;;  (list
@@ -23,15 +17,15 @@
 ;;   (list 'ingame (rs-read "ingame.wav"))))
  
 ;;(define (play-forever sound)
-  ;(make-pstream [#:buffer-time buffer-time]) → pstream? Create a new pstream and start playing it.
-;;  (define play (make-pstream))
-  ;(rs-frames sound) → nonnegative-integer? Returns the length of a sound, in frames.
-;;  (define length (rs-frames sound))
-;;  (let looping ((time 1))
-    ;(pstream-queue pstream rsound frames) → pstream? Queue the given sound to be played at the time specified by frames.
-;;    (pstream-queue play sound (+ time 44100))
-;;    (define next (+ time length))
-;;    (looping next)))
+  ;;(make-pstream [#:buffer-time buffer-time]) → pstream? Create a new pstream and start playing it.
+  ;;(define play (make-pstream))
+  ;;(rs-frames sound) → nonnegative-integer? Returns the length of a sound, in frames.
+  ;;(define length (rs-frames sound))
+  ;;(let looping ((time 1))
+    ;;(pstream-queue pstream rsound frames) → pstream? Queue the given sound to be played at the time specified by frames.
+    ;;(pstream-queue play sound (+ time 44100))
+    ;;(define next (+ time length))
+    ;;(looping next)))
 
 ;;(play-forever play) 
 
